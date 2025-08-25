8a667aa89209caf2f23acaae1dae8ac031fe3bbe
Trippsterx <115191407+Trippsterx@users.noreply.github.com>
2025-08-25 15:10:57 -0500

    Sheet 2

---
+++ Tripps Charachter Sheet.py
@@ -0,0 +1,39 @@
+# Tripps Charachter Sheet
+name = "Tripp Totten"
+race = "Out Worlder"
+nature = "Undefined"
+print(name)
+print(race)
+print(nature)
+# Strength Info
+strength = "Strength 9"
+dexterity = "Dexterity 12"
+print(strength)
+print(dexterity)
+# Health Info
+health = "Health 100/100"
+constitution = "Constitution 9"
+print(health)
+print(constitution)
+# Brein Info
+wisdom = "Wisdom 6"
+inteligence = " Inteligence 8"
+charisma = "Charisma 4"
+print(wisdom)
+print(inteligence)
+print(charisma)
+# Magic Info
+mana = "Mana 45/45"
+print(mana)
+# Class Info
+Class = "Class - Duelist lvl 3"
+ArmorClass = "Armor Class - Undefined"
+print(Class)
+print(ArmorClass)
+#  Skills
+profiecncies = 
+print(profiecncies)
+
+
+
+
