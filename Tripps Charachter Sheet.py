a4bb6e9b9fa6d13330d9d0c8dd5720b3bc1e8b8e
Trippsterx <115191407+Trippsterx@users.noreply.github.com>
2025-08-26 09:02:23 -0500

    Testing

--- Tripps Charachter Sheet.py
+++ Tripps Charachter Sheet.py
@@ -1,39 +1,35 @@
 # Tripps Charachter Sheet
 name = "Tripp Totten"
-race = "Out Worlder"
-nature = "Undefined"
+race = "Race - Out Worlder"
+nature = "Nature - Undefined"
 print(name)
 print(race)
 print(nature)
 # Strength Info
-strength = "Strength 9"
-dexterity = "Dexterity 12"
+strength = "Strength - 9"
+dexterity = "Dexterity - 12"
 print(strength)
 print(dexterity)
 # Health Info
-health = "Health 100/100"
-constitution = "Constitution 9"
+health = "Health - 100/100"
+constitution = "Constitution - 9"
 print(health)
 print(constitution)
 # Brein Info
 wisdom = "Wisdom 6"
-inteligence = " Inteligence 8"
-charisma = "Charisma 4"
+inteligence = "Inteligence - 8"
+charisma = "Charisma - 4"
 print(wisdom)
 print(inteligence)
 print(charisma)
 # Magic Info
-mana = "Mana 45/45"
+mana = "Mana - 45/45"
 print(mana)
 # Class Info
 Class = "Class - Duelist lvl 3"
 ArmorClass = "Armor Class - Undefined"
 print(Class)
 print(ArmorClass)
-#  Skills
-profiecncies = 
+# Skills
+profiecncies = "None"
 print(profiecncies)
-
-
-
-
